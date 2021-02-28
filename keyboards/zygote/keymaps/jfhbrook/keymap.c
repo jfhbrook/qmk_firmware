941b5da262bebf6f4d248a8577a55fb8380934ce
Josh Holbrook <josh.holbrook@gmail.com>
2021-02-27 19:59:03 -0500

    Fix backslash

--- keyboards/zygote/keymaps/jfhbrook/keymap.c
+++ keyboards/zygote/keymaps/jfhbrook/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_ortho_4x12( \
   KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,         KC_NO,    KC_NO,           KC_Y,    KC_U,    KC_I,    KC_O  ,       KC_P,  \
   KC_A,    KC_S,    KC_D,    KC_F,      KC_G,         KC_NO,    KC_NO,           KC_H,    KC_J,    KC_K,    KC_L  ,    KC_SCLN,  \
-  KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,       KC_QUOT,  KC_PIPE,           KC_N,    KC_M,    KC_COMM, KC_DOT,    KC_SLSH,  \
+  KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,       KC_QUOT,  KC_BSLS,           KC_N,    KC_M,    KC_COMM, KC_DOT,    KC_SLSH,  \
   KC_ESC,  KC_TAB,  KC_LGUI, KC_LSFT,   KC_BSPC,    KC_LCTL,  KC_LALT,         KC_SPC,   LOWER,    KC_MINS,  RAISE,     KC_ENT   \
 ),
 
